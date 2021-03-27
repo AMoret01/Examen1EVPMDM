32ee186c781ed9618bfdf54278a1c1b49cc7a569
AMoret01 <almogo01@floridauniversitaria.es>
2021-03-27 21:54:11 +0100

    He consegido que elimine la letra que le pongo pero no la palabra entera

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-var texto = 'Lorem ipsum dolor sit amet consectetur adipiscing elit gravida cras proin justo, diam pulvinar in leo aenean morbi nunc lobortis nibh Sollicitudin mattis turpis neque condimentum posuere volutpat dignissim cras, platea egestas pharetra etiam sagittis a et, habitasse placerat mi malesuada facilisis velit ligula. Lacinia cras justo cum viverra gravida egestas neque dictum, auctor sociosqu mauris lacus erat placerat condimentum, varius fames praesent laoreet est donec suscipit';
+const texto = 'Lorem ipsum dolor sit amet consectetur adipiscing elit gravida cras proin justo, diam pulvinar in leo aenean morbi nunc lobortis nibh Sollicitudin mattis turpis neque condimentum posuere volutpat dignissim cras, platea egestas pharetra etiam sagittis a et, habitasse placerat mi malesuada facilisis velit ligula. Lacinia cras justo cum viverra gravida egestas neque dictum, auctor sociosqu mauris lacus erat placerat condimentum, varius fames praesent laoreet est donec suscipit';
 var espacio = " ";
 
-const dividirTexto = (cadenaADividir,separador) => {
-    var array = cadenaADividir.split(separador);
-    console.log("<br>El array tiene " + array.length + " elementos: ");
+ const dividirTexto = (cadenaADividir,separador) => {
+     var array = cadenaADividir.split(separador);
  
-    for (var i=0; i < array.length; i++) {
-       console.log(array[i] + " / ");
+     for (var i=0; i < array.length; i++) {
+        console.log(array[i] + " / ");
     }
- }
- dividirTexto(texto, espacio);
+      return array;
+  }
+//  dividirTexto(texto, espacio);
 
 
- const aceptar = document.createElement('button');
-    aceptar.textContent = "Aceptar";
-    aceptar.addEventListener('click', /*funcionfinal*/);
+//  const aceptar = document.createElement('button');
+//     aceptar.textContent = "Aceptar";
+//     aceptar.addEventListener('click', /*funcionfinal*/);
+const cadena = dividirTexto(texto, espacio)
+const letra = 'c'
+const filterText = (cadena,letra) => {
+   return cadena.filter(item => !letra.includes(item));
+}
 
+console.log(filterText(cadena,letra));
