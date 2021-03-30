25262be035259b935deb08b2a160efe4243c13b0
AMoret01 <almogo01@floridauniversitaria.es>
2021-03-30 10:38:34 +0200

    Ejercicio 1 finalizado

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 const texto = 'Lorem ipsum dolor sit amet consectetur adipiscing elit gravida cras proin justo, diam pulvinar in leo aenean morbi nunc lobortis nibh Sollicitudin mattis turpis neque condimentum posuere volutpat dignissim cras, platea egestas pharetra etiam sagittis a et, habitasse placerat mi malesuada facilisis velit ligula. Lacinia cras justo cum viverra gravida egestas neque dictum, auctor sociosqu mauris lacus erat placerat condimentum, varius fames praesent laoreet est donec suscipit';
-var espacio = " ";
+const caracter = 'c';
+const textoDividido = texto.split(" ");
+
+const filtrarPalabras = (cadenaDeTexto, caracterARemplazar) => {
+
+  const filtradas = [];
+  cadenaDeTexto.forEach(palabra => {
+    if(!palabra.includes(caracterARemplazar)){
+        console.log("Ha entrado");
+        filtradas.push(palabra);
+    }
+  });
+
+  // console.log(filtradas);  
+  return filtradas;
+}
+
+const filterTextOnKeyUp = () => {
+  const textElement = document.getElementById("texto");
+  const textoValue = textElement.innerText.split(" ");
+  const caracter = document.getElementById("filterText").value;
+  textElement.innerText = filtrarPalabras(textoValue, caracter).join(" ") || texto;
+}
+
+// console.log(filtrarPalabras(textoDividido,caracter).join(" "));
 
  const dividirTexto = (cadenaADividir,separador) => {
      var array = cadenaADividir.split(separador);
  
-     for (var i=0; i < array.length; i++) {
-        console.log(array[i] + " / ");
-    }
+    //  for (var i=0; i < array.length; i++) {
+    //     console.log(array[i] + " / ");
+    // }
       return array;
   }
-//  dividirTexto(texto, espacio);
+// //  dividirTexto(texto, espacio);
 
 
-//  const aceptar = document.createElement('button');
-//     aceptar.textContent = "Aceptar";
-//     aceptar.addEventListener('click', /*funcionfinal*/);
-const cadena = dividirTexto(texto, espacio)
-const letra = 'c'
-const filterText = (cadena,letra) => {
-   return cadena.filter(item => !letra.includes(item));
-}
+// //  const aceptar = document.createElement('button');
+// //     aceptar.textContent = "Aceptar";
+// //     aceptar.addEventListener('click', /*funcionfinal*/);
+// const cadena = dividirTexto(texto, espacio)
+// const letra = 'c'
+// const filterText = (cadena,letra) => {
+//    return cadena.filter(item => !letra.includes(item));
+// }
 
-console.log(filterText(cadena,letra));
+// console.log(filterText(cadena,letra));
